421a60eb0b8e6c9ed654acef29efe03e683f2fb5
pranjal23030 <pranjal.khatiwoda@gmail.com>
2024-09-24 19:33:16 +0545

    blog-table-creation

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Blogs(models.Model):
+    title = models.CharField(max_length=25)
+    subtitle = models.CharField(max_length=255,blank=False, null=False)
+    description = models.TextField()
+    image = models.ImageField(upload_to='images', blank=True, null=True)
+    created_at = models.DateField(auto_now_add=True)
+
+
