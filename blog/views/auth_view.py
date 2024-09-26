5514e5c5d52b40379c97b3bfe4066b2bdeb98cfe
pranjal23030 <pranjal.khatiwoda@gmail.com>
2024-09-26 19:52:43 +0545

    register-create-logic

--- blog/views/auth_view.py
+++ blog/views/auth_view.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 def register(request):
-    return render(request,"auth/register.html")
+    if request.method == 'POST':
+        email = request.POST.get("email")
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = User.objects.create(
+            username = username, 
+            email = email, 
+        )
+        user.set_password(password)
+        user.save()
+        return redirect("login")
+    return render(request,'auth/register.html')
 
 def login(request):
-    return render(request,"auth/login.html")
-
+    return render(request,'auth/login.html')
--- blog/views/main_view.py
+++ blog/views/main_view.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from ..models import Blogs
 
 # Create your views here.
 
@@ -9,6 +10,14 @@ def single_blog(request):
     return render(request,'main/single_blog.html')
 
 def create_blog(request):
+    if request.method == "POST":
+        title = request.POST.get("title"),
+        subtitle = request.POST.get("subtitle")
+        description = request.POST.get("description")
+        blog = Blogs(title=title, subtitle=subtitle, description=description)
+        blog.save()
+        return redirect("home")
+
     return render(request,'main/create_blog.html')
 
 def edit_blog(request):
