47ec3fab531ddba7b60d541a1d11e3f02e1d36e8
pranjal23030 <pranjal.khatiwoda@gmail.com>
2024-09-30 18:04:57 +0545

    login-page-logic

--- blog/views/main_view.py
+++ blog/views/main_view.py
@@ -1,25 +1,26 @@
 from django.shortcuts import render,redirect
 from ..models import Blogs
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
 
 def home(request):
     return render(request,'main/home.html')
 
 def single_blog(request):
-    return render(request,'main/single_blog.html')
-
-def create_blog(request):
-    if request.method == "POST":
-        title = request.POST.get("title"),
-        subtitle = request.POST.get("subtitle")
-        description = request.POST.get("description")
-        blog = Blogs(title=title, subtitle=subtitle, description=description)
-        blog.save()
-        return redirect("home")
-
-    return render(request,'main/create_blog.html')
+    return render(request,"main/single_blog.html")
 
 def edit_blog(request):
-    return render(request,'main/edit_blog.html')
+    return render(request,"main/edit_blog.html")
 
+@login_required
+def create_blog(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        subtitle =  request.POST.get("subtitle")
+        description = request.POST.get("description")
+        image = request.FILES.get("image")
+        blog = Blogs(title=title,subtitle=subtitle,description=description,image=image)
+        blog.save()
+        return redirect("home")
+  
+    return render(request,"main/create_blog.html")
