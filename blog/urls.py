91789e4678707219597d0e16e3e9d74b64f9680b
pranjal23030 <pranjal.khatiwoda@gmail.com>
2024-10-03 22:50:06 +0545

    delete-url

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views.auth_view import register, login
-from .views.main_view import home,create_blog, single_blog, edit_blog
+from .views.main_view import home,create_blog, single_blog, edit_blog, delete_blog
 
 
 urlpatterns = [
@@ -8,8 +8,8 @@ urlpatterns = [
     path("register/",register, name="register"),
     path("login/",login, name ="login"),
     path("create/",create_blog),
-    path("single/",single_blog),
-    path("edit/",edit_blog)
-
+    path("<int:blog_id>",single_blog),
+    path("edit/",edit_blog),
+    path("<int:blog_id>/delete",delete_blog,name="delete_blog")
 ]
 
