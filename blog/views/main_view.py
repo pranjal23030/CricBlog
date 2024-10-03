33d79c58dd2ebb315b2801821d420c2def257890
pranjal23030 <pranjal.khatiwoda@gmail.com>
2024-10-03 22:49:03 +0545

    single-read-and-delete

--- blog/views/main_view.py
+++ blog/views/main_view.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from ..models import Blogs
 from django.contrib.auth.decorators import login_required
 
@@ -9,8 +9,11 @@ def home(request):
         'blogs' : blogs
     })
 
-def single_blog(request):
-    return render(request,"main/single_blog.html")
+def single_blog(request, blog_id):
+    blog = get_object_or_404(Blogs,pk=blog_id);
+    return render(request,"main/single_blog.html", {
+        "blog" : blog
+    })
 
 def edit_blog(request):
     return render(request,"main/edit_blog.html")
@@ -27,3 +30,10 @@ def create_blog(request):
         return redirect("home")
   
     return render(request,"main/create_blog.html")
+
+
+def delete_blog(request,blog_id):
+    blog = get_object_or_404(Blogs,pk=blog_id)
+    blog.delete()
+    return redirect("home")
+
