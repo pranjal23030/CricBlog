062b5493cc89efaeed9769653ee635b6adba18a0
pranjal23030 <pranjal.khatiwoda@gmail.com>
2024-10-12 21:09:19 +0545

    edit logic

--- blog/views/main_view.py
+++ blog/views/main_view.py
@@ -15,8 +15,28 @@ def single_blog(request, blog_id):
         "blog" : blog
     })
 
-def edit_blog(request):
-    return render(request,"main/edit_blog.html")
+@login_required
+def edit_blog(request, blog_id):
+    blog = get_object_or_404(Blogs, pk=blog_id)
+    
+    if blog.author != request.user:
+        return redirect("home")
+
+    if request.method == "POST":
+        blog.title = request.POST.get("title")
+        blog.subtitle = request.POST.get("subtitle")
+        blog.description = request.POST.get("description")
+        
+        if request.FILES.get("image"):
+            blog.image = request.FILES.get("image")
+
+        blog.save()
+        return redirect("blog_detail", blog_id=blog.id)
+    
+    return render(request, "main/edit_blog.html", {
+        "blog": blog # Giving blog data to edit_blog.html
+    })
+
 
 @login_required
 def create_blog(request):
