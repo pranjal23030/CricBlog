a41488525ce502d75b09947e3bbf608f6200ec06
pranjal23030 <pranjal.khatiwoda@gmail.com>
2024-10-31 20:09:48 +0545

    error check

--- blog/views/auth_view.py
+++ blog/views/auth_view.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login as LOGIN
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == 'POST':
@@ -17,18 +18,16 @@ def register(request):
     return render(request,'auth/register.html')
 
 def login(request):
-    if request.method == 'POST': 
-        username = request.POST.get("username") 
-        password = request.POST.get("password") 
-        user = authenticate(request,username=username,password=password) 
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
         if user is not None:
-            LOGIN(request,user)
+            LOGIN(request, user)
             return redirect("home")
         else:
-            return redirect("login")
-    return render(request,'auth/login.html')
-
-from django.contrib.auth.decorators import login_required
+            return render(request, 'auth/login.html', {'error': 'Invalid username or password'})
+    return render(request, 'auth/login.html')
 
 @login_required
 def profile(request):
